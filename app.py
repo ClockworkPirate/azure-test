970c204fc55fb528242d9b969999624b57f49171
Mario Guerrieri <mguerrieri@outlook.com>
2015-08-30 14:27:06 -0700

    Trying stuff out

---
+++ app.py
@@ -0,0 +1,62 @@
+import cherrypy
+
+wsgi_app = cherrypy.Application(AzureTest(), '/')
+
+class AzureTest(object):
+	@cherrypy.expose
+	def index(self):
+		return open("index.html", "r").read()
+
+# class RobotControlAPI(object):
+# 	exposed = True
+
+# 	@cherrypy.tools.accept(media='text/plain')
+# 	def POST(self, direction):
+# 		print "Recieved control."
+# 		if direction == "f":
+# 			print "Moving forward."
+# 			digitalWrite(l, HIGH)
+# 			digitalWrite(r, HIGH)
+# 			delay(100)
+# 		elif direction == "l":
+# 			print "Turning left."
+# 			digitalWrite(l, LOW)
+# 			digitalWrite(r, HIGH)
+# 			delay(100)
+# 		elif direction == "r":
+# 			print "Turning right."
+# 			digitalWrite(l, HIGH)
+# 			digitalWrite(r, LOW)
+# 			delay(100)
+# 		else:
+# 			print "Stopping."
+# 			digitalWrite(l, LOW)
+# 			digitalWrite(r, LOW)
+
+# if __name__ == '__main__':
+# 	l = 4
+# 	r = 2
+	
+# 	pinMode(l, OUTPUT)
+# 	pinMode(r, OUTPUT)
+	
+# 	conf = {
+# 		'/': {
+# 			'tools.staticdir.root': os.path.abspath(os.getcwd())
+# 		},
+# 		'/control': {
+# 			'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+# 			'tools.response_headers.on': True,
+# 			'tools.response_headers.headers': [('Content-Type', 'text/plain')],
+# 		},
+# 		'/static': {
+# 			'tools.staticdir.on': True,
+# 			'tools.staticdir.dir': './public'
+# 		}
+# 	}
+	
+# 	cherrypy.server.socket_host = '0.0.0.0'
+	
+# 	page = RobotControl()
+# 	page.control = RobotControlAPI()
+# 	cherrypy.quickstart(page, '/', conf)
